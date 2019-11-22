9994c8cacfd569ada73e6c6c65fa654cf1aadd77
LiChunqiang <lcqforwork@163.com>
2019-11-22 09:13:07 +0800

    ReverseInteger

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        ZigZagConversion solution = new ZigZagConversion();
-        System.out.println(solution.convert("ABCDEFGHIJKLMN",3));
+        ReverseInteger solution = new ReverseInteger();
+        System.out.println(solution.reverse(1534236469));
     }
 }
