84b63d4816439fe784a1678f28e0cf5229c78217
LiChunqiang <lcqforwork@163.com>
2019-12-03 17:19:48 +0800

    Remove Nth Node From End of List

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        KSumSolution solution = new KSumSolution();
-        System.out.println(solution.kSum(new int[]{-1, 0, -5, -2, -2, -4, 0, 1, -2}, -9, 4));
+        RemoveNthNodeFromEndOfList solution = new RemoveNthNodeFromEndOfList();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        System.out.println(solution.removeNthFromEnd(head, 2));
     }
 }
---
+++ src/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,31 @@
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int i = 0;
+        ListNode pointer = head;
+        ListNode delayedPointer = null;
+        while (pointer != null) {
+            pointer = pointer.next;
+            i++;
+            if (delayedPointer != null) {
+                delayedPointer = delayedPointer.next;
+            }
+            if (i == n + 1) {
+                //延迟n+1步  使指针指向被删除节点前一个
+                delayedPointer = head;
+            }
+        }
+        if (i == n) {
+            //当被删除节点为头结点时  头结点后移
+            head = head.next;
+            return head;
+        } else if (delayedPointer == null) {
+            //即n大于链表长度时  不删除任何节点
+            return head;
+        }
+        delayedPointer.next = delayedPointer.next.next;
+        return head;
+    }
+
+}
+
+
