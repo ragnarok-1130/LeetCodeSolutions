bcdd3672d4c440a3504e8bc39f8b67ab9cfc018b
LiChunqiang <lcqforwork@163.com>
2019-12-03 18:01:41 +0800

    Valid Parentheses

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        RemoveNthNodeFromEndOfList solution = new RemoveNthNodeFromEndOfList();
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        System.out.println(solution.removeNthFromEnd(head, 2));
+        ValidParentheses solution = new ValidParentheses();
+        System.out.println(solution.isValid("(([]){})"));
     }
 }
---
+++ src/ValidParentheses.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> leftParentheses = new Stack<>();
+        for (char ch : chars) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                leftParentheses.push(ch);
+            } else if (ch == ')') {
+                if (leftParentheses.size() == 0 || leftParentheses.pop() != '(') {
+                    return false;
+                }
+            } else if (ch == ']') {
+                if (leftParentheses.size() == 0 || leftParentheses.pop() != '[') {
+                    return false;
+                }
+            } else if (ch == '}') {
+                if (leftParentheses.size() == 0 || leftParentheses.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        if (leftParentheses.size() != 0) {
+            return false;
+        }
+        return true;
+    }
+}
