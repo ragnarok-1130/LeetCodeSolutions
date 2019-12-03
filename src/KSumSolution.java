9c75e522d0f0ac4a6ad5efed1f063f63c340e8b1
LiChunqiang <lcqforwork@163.com>
2019-12-03 16:16:47 +0800

    K Sum Solution

---
+++ src/KSumSolution.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class KSumSolution {
+    public List<List<Integer>> kSum(int[] nums, int target, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < k || k < 2) {
+            return result;
+        }
+        //国际惯例 先排序
+        Arrays.sort(nums);
+        return kSum(nums, target, k, 0);
+    }
+
+    private List<List<Integer>> kSum(int[] nums, int target, int k, int start) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (k == 2) {
+            int left = start;
+            int right = nums.length - 1;
+            while (left < right) {
+                //跳过重复值
+                if (left > start && nums[left] == nums[left - 1]) {
+                    left++;
+                    continue;
+                }
+                if (right < nums.length - 1 && nums[right] == nums[right + 1]) {
+                    right--;
+                    continue;
+                }
+                if (nums[left] + nums[right] == target) {
+                    result.add(Arrays.asList(nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+            return result;
+        } else {
+            for (int i = start; i <= nums.length - k; i++) {
+                if (i > start && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if (nums[i] * k > target || nums[nums.length - 1] < target) {
+                    return result;
+                }
+                List<List<Integer>> kMinusOneResult = kSum(nums, target - nums[i], k - 1, i + 1);
+                if (kMinusOneResult.size() != 0) {
+                    for (List<Integer> kMinusOne : kMinusOneResult) {
+                        List<Integer> combine = new ArrayList<>();
+                        combine.add(nums[i]);
+                        combine.addAll(kMinusOne);
+                        result.add(combine);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        FourSum solution = new FourSum();
-        System.out.println(solution.fourSum(new int[]{-1,0,-5,-2,-2,-4,0,1,-2}, -9));
+        KSumSolution solution = new KSumSolution();
+        System.out.println(solution.kSum(new int[]{-1, 0, -5, -2, -2, -4, 0, 1, -2}, -9, 4));
     }
 }
