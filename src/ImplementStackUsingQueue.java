eb46a145aa2094e2a27292cd21101cf1b776db0f
Lee <848600266@qq.com>
2021-05-25 22:26:16 +0800

    使用队列实现先进后出的栈

---
+++ src/ImplementStackUsingQueue.java
@@ -0,0 +1,88 @@
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ImplementStackUsingQueue {
+
+    static class MyStack {
+
+        /**
+         * 使用两个队列实现先进后出
+         */
+        private Queue<Integer> q1, q2;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyStack() {
+            // 初始化队列
+            q1 = new ArrayBlockingQueue<>(100);
+            q2 = new ArrayBlockingQueue<>(100);
+        }
+
+        /**
+         * Push element x onto stack.
+         */
+        public void push(int x) {
+            if (q1.isEmpty()) {
+                q1.add(x);
+                while (!q2.isEmpty()) {
+                    // 将另一个队列所有值移动到此队列  使得最后进入的元素位于队列第一个  实现先进后出
+                    q1.add(q2.poll());
+                }
+            } else {
+                // q2为空时
+                q2.add(x);
+                while (!q1.isEmpty()) {
+                    // 将另一个队列所有值移动到此队列  使得最后进入的元素位于队列第一个  实现先进后出
+                    q2.add(q1.poll());
+                }
+            }
+        }
+
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
+        public int pop() {
+            if (q1.isEmpty() && q2.isEmpty()) {
+                return -1;
+            }
+            if (q1.isEmpty()) {
+                return q2.poll();
+            } else {
+                return q1.poll();
+            }
+        }
+
+        /**
+         * Get the top element.
+         */
+        public int top() {
+            if (q1.isEmpty() && q2.isEmpty()) {
+                return -1;
+            }
+            int val;
+            if (q1.isEmpty()) {
+                val = q2.poll();
+                q1.add(val);
+                while (!q2.isEmpty()) {
+                    q1.add(q2.poll());
+                }
+            } else {
+                val = q1.poll();
+                q2.add(val);
+                while (!q1.isEmpty()) {
+                    q2.add(q1.poll());
+                }
+            }
+            return val;
+        }
+
+        /**
+         * Returns whether the stack is empty.
+         */
+        public boolean empty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+    }
+
+}
